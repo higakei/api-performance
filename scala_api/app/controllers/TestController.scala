98bd44ecb72d5336c0a29f2f3a93b7980d84b03b
higakei <higakei@hotmail.com>
2017-08-09 16:30:27 +0900

    playにtest2を追加

--- scala_api/app/controllers/TestController.scala
+++ scala_api/app/controllers/TestController.scala
@@ -3,7 +3,10 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import play.api.libs.json._
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, AnyContent, Controller}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class TestController @Inject() extends Controller {
@@ -11,5 +14,31 @@ class TestController @Inject() extends Controller {
   def index = Action {
     Ok(Json.obj("result" -> "すいようのどようのうしのひ"))
       .as("application/json; charset=utf-8")
+  }
+
+  def indexByAsync: Action[AnyContent] = Action.async {
+    Future {
+      Ok(Json.obj("result" -> "すいようのどようのうしのひ"))
+        .as("application/json; charset=utf-8")
+    }
+  }
+
+  def test2(size: Int) = Action {
+    Ok(createJson(size)).as("application/json; charset=utf-8")
+  }
+
+  def test2ByAsync(size: Int): Action[AnyContent] = Action.async {
+    Future {
+      Ok(createJson(size)).as("application/json; charset=utf-8")
+    }
+  }
+
+  private def createJson(size: Int): JsObject = {
+    val list = (1 to size).map { i =>
+      Map(s"key$i" -> s"value$i")
+    }
+    val json = Json.obj("results" -> list)
+    println(s"size $size:${json.toString().getBytes.length} bytes")
+    json
   }
 }
